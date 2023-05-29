80747919ba750d92fe86c69d21148e3b95611ef2
Brianmahlatse <mahlatsebrian04@gmail.com>
2023-05-29 14:43:22 +0200

    changed vite file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import handlebars from 'vite-plugin-handlebars';
-import data from './data.json';
+import data from './data.json' assert { type: 'json' };
+import { defineConfig } from 'vite';
 
-export default{
-    plugins:[
-        handlebars(
-            {
-                context:data
-            }
-        )
-    ]
-}
+export default defineConfig({
+  base: '/responsive_webpages/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        bootstrap: 'index-bootstrap.html',
+        materialize: 'index-materialize.html',
+      },
+    },
+  },
+  plugins: [handlebars({ context: data })],
+});
