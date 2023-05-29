9bce882f55425e72776a5cb03f215508fcbf18e0
Brianmahlatse <mahlatsebrian04@gmail.com>
2023-05-29 13:49:00 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function goToGithub(){
+    window.location.href="https://github.com/Brianmahlatse";
+}
+function goToLinkedIn(){
+    window.location.href="https://www.linkedin.com/in/brian-mahlatse-malau/";
+}
+
+// JavaScript to hide loading page and show content after a delay
+setTimeout(function() {
+document.getElementById('loading-page').style.display = 'none';
+document.getElementById('content').style.display = 'block';
+}, 1800); // delay time (in milliseconds)
