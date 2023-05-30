2f687131ac8a64abffceb16d0f4c539bcd421e7e
Brianmahlatse <mahlatsebrian04@gmail.com>
2023-05-30 16:40:33 +0200

    move main.js code tp html files

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,1 @@
-function bootstrap(){
-    window.location.href="index-bootstrap.html";
-};
-function materialize(){
-    window.location.href="index-materialize.html";
-}
 
-function goToGithub(){
-    window.location.href="https://github.com/Brianmahlatse";
-}
-function goToLinkedIn(){
-    window.location.href="https://www.linkedin.com/in/brian-mahlatse-malau/";
-}
-
-// JavaScript to hide loading page and show content after a delay
-setTimeout(function() {
-document.getElementById('loading-page').style.display = 'none';
-document.getElementById('content').style.display = 'block';
-}, 1800); // delay time (in milliseconds)
