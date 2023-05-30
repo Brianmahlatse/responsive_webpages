711a72e73e3da537ebd3000c3e2260555b8d7646
Brianmahlatse <mahlatsebrian04@gmail.com>
2023-05-30 13:00:04 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/responsive_webpages/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        bootstrap: 'index-bootstrap.html',
-        materialize: 'index-materialize.html',
-      },
-    },
-  },
+
+export default ({
+
   plugins: [handlebars({ context: data })],
 });
